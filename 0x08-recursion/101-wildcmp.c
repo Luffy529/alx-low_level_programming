ae5154640bdc6739cc347d2cd97672999b18a641
Luffy529 <youssefnafaa529@gmail.com>
2023-09-01 09:02:48 +0100

    ok

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1
- * if the strings can be considered identical,
- * otherwise return 0.
- * @str1: the normal string
- * @str2: the special string containing "*"
- *
- * Return: 1 if identical, else 0
- */
+* wildcmp - compares two strings and returns 1
+* if the strings can be considered identical,
+* otherwise return 0.
+* @str1: the normal string
+* @str2: the special string containing "*"
+*
+* Return: 1 if identical, else 0
+*/
 int wildcmp(char *str1, char *str2)
 {
 	if (*str2 == '\0' && *str1 == '\0')
